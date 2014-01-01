164448a43819771461dc38c64a3abd8bb8b0bfce
Hugo Benichi <hugo@midokura.com>
2014-01-02 01:51:32 +0900

    Monoid#concat

--- src/Monoid.java
+++ src/Monoid.java
@@ -3,4 +3,13 @@
 public interface Monoid<M extends Monoid<M>> {
   public M union(M that);
   public M empty();
+/*
+  java 8
+  public M concat(Iterable<M> ms);
+  default public M concat(Iterable<M> ms) {
+    M ret = empty();
+    for (M m : ms) { ret = ret.union(m); }
+    return ret;
+  }
+*/
 }
